276185bc566aedd91acdba66b2eedd84b79f1f95
Ben Cornelis <bencornelis02@gmail.com>
2015-05-01 10:27:47 -0700

    display newly added word in dictionary page

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -3,6 +3,11 @@ require('definition')
 require('word')
 
 describe(Word) do
+
+  before () do
+    Word.clear()
+  end
+  
   describe('#word') do
     it('returns the word') do
       test_word = Word.new({:word => 'run'})
